e4651320a0d7e210f8b0614b1718ad82017d7031
Shubham Gupta <shubham@Shubhams-MacBook-Pro.local>
2024-07-19 23:59:43 +0530

    code init

---
+++ src/main/java/com/gupshub/usermanagement/GupshubUsermgtApplication.java
@@ -0,0 +1,13 @@
+package com.gupshub.usermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GupshubUsermgtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GupshubUsermgtApplication.class, args);
+	}
+
+}
